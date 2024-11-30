649e4852d7baa4a08947b007d129ec1d371b8ba6
naman23j <140912648+naman23j@users.noreply.github.com>
2024-11-30 14:34:56 +0530

    Add files via upload

---
+++ amazon_clone/script.js
@@ -0,0 +1,22 @@
+// let x = null;
+// var a = "Tammy";
+// var found = false;
+
+// // => Tammy, false, null
+// console.log(a, found, x);
+
+// var ans = "naman";
+// console.log(ans); 
+
+// let number = 100;
+
+// // Both statements will add 10
+// number = number + 10;
+// number +=10;
+
+// console.log(number); 
+// // => 120
+const numberOfColumns = 4;
+
+// TypeError: Assignment to constant...
+numberOfColumns = 8;
